1a6e9729ffd2d745c63e74a84774f5d91979c13a
matonoha <matonohamartin@gmail.com>
2020-08-18 13:14:11 +0200

    syntax correct enums init

--- instructions.py
+++ instructions.py
@@ -1,19 +1,20 @@
 from enum import Enum
+from enum import auto
 
 class Instructions(Enum):
 	OP_BR = 0,
-	OP_ADD,
-	OP_LD,
-	OP_ST,
-	OP_JSR,
-	OP_AND,
-	OP_LDR,
-	OP_STR,
-	OP_RTI,
-	OP_NOT,
-	OP_LDI,
-	OP_STI,
-	OP_JMP,
-	OP_RES,
-	OP_LEA,
-	OP_TRAP	
+	OP_ADD = auto()
+	OP_LD = auto()
+	OP_ST = auto()
+	OP_JSR = auto()
+	OP_AND = auto()
+	OP_LDR = auto()
+	OP_STR = auto()
+	OP_RTI = auto()
+	OP_NOT = auto()
+	OP_LDI = auto()
+	OP_STI = auto()
+	OP_JMP = auto()
+	OP_RES = auto()
+	OP_LEA = auto()
+	OP_TRAP	= auto()
--- registers.py
+++ registers.py
@@ -1,13 +1,14 @@
 from enum import Enum
+from enum import auto
 
 class Registers(Enum):
-  R_R0 = 0,
-  R_R1,
-  R_R2,
-  R_R4,
-  R_R5,
-  R_R6,
-  R_R7,
-  R_PC, # program counter
-  R_COND,
-  R_COUNT 
+  R_R0 = 0
+  R_R1 = auto()
+  R_R2 = auto()
+  R_R4 = auto()
+  R_R5 = auto()
+  R_R6 = auto()
+  R_R7 = auto()
+  R_PC = auto() # program counter
+  R_COND = auto()
+  R_COUNT = auto()
