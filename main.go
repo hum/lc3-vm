9241737004785afe5cfc8405ad875c04f5bfb334
hum <65712791+hum@users.noreply.github.com>
2022-03-08 19:58:31 +0100

    read args

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"fmt"
 	lc3 "github.com/hum/lc3-vm/vm"
+	"os"
 )
 
 func main() {
+	args := os.Args[1:]
+	fmt.Println(args)
+
+	for arg := range args {
+		fmt.Println("File: %s", arg)
+		// TODO: check if exists and read the assembly from the file
+	}
+
 	var ram *lc3.RAM = lc3.GetRAM()
 	var cpu *lc3.CPU = lc3.GetCPU(ram)
 	cpu.Run()
